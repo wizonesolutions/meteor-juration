7e80201df48a8cd77994d97a11bc585c5fd54423
Kevin Kaland <kevin@wizonesolutions.com>
2015-07-27 00:22:48 +0200

    Update to new package system.

--- package.js
+++ package.js
@@ -1,8 +1,11 @@
 Package.describe({
-  summary: 'A simple natural-language duration parser.'
+  summary: 'A simple natural-language duration parser.',
+  version: "1.0.0",
+  git: "https://github.com/wizonesolutions/meteor-juration.git"
 });
 
 Package.on_use(function (api, where) {
+  api.versionsFrom("METEOR@0.9.0");
   where = where || ['client', 'server'];
 
   api.add_files(['pre.js'], where);
